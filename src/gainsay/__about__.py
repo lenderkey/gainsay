7e5aee531c1a00f720c10161f7c8af2d3a0bcc75
David Janes <davidjanes@gmail.com>
2025-01-07 13:22:21 -0500

    ...

---
+++ src/gainsay/__about__.py
@@ -0,0 +1,7 @@
+__title__ = 'gainsay'
+__description__ = 'Redis Notification Framework.'
+__version__ = '0.1.0'
+__author__ = 'David Janes'
+__author_email__ = 'davidjanes@gmail.com'
+# __license__ = 'MIT'
+__url__ = 'https://github.com/lenderkey/gainsay'
